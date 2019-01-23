17e2ae6708270e920864d7a06abd0f32c5fd2c16
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-01-22 19:44:22 -0600

    Added PlaceholderApiReplacer class for messages PlaceholderAPI support without errors

---
+++ src/main/java/us/sparknetwork/base/listeners/PlaceholderApiReplacer.java
@@ -0,0 +1,15 @@
+package us.sparknetwork.base.listeners;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.entity.Player;
+
+public class PlaceholderApiReplacer {
+
+    public static String parsePlaceholders(Player player, String text){
+        return PlaceholderAPI.setPlaceholders(player, text);
+    }
+
+    public static String parseRelationalPlaceholders(Player player1, Player player2, String text){
+        return PlaceholderAPI.setRelationalPlaceholders(player1, player2, text);
+    }
+}
