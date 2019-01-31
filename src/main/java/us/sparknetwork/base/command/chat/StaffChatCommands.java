fc70321b6e19a1190fc5e042079758cac7de8663
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-01-30 22:06:33 -0600

    Add /sc <text> support

--- src/main/java/us/sparknetwork/base/command/chat/StaffChatCommands.java
+++ src/main/java/us/sparknetwork/base/command/chat/StaffChatCommands.java
@@ -1,9 +1,15 @@
 package us.sparknetwork.base.command.chat;
 
 import com.google.inject.Inject;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import us.sparknetwork.base.I18n;
 import us.sparknetwork.base.LangConfigurations;
+import us.sparknetwork.base.messager.Channel;
+import us.sparknetwork.base.messager.Messenger;
+import us.sparknetwork.base.messager.messages.StaffChatMessage;
 import us.sparknetwork.base.user.User;
 import us.sparknetwork.base.user.UserHandler;
 import us.sparknetwork.cm.CommandClass;
@@ -11,7 +17,13 @@ import us.sparknetwork.cm.annotation.Command;
 import us.sparknetwork.cm.command.arguments.CommandContext;
 
 import java.text.MessageFormat;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static us.sparknetwork.utils.ListenableFutureUtils.addCallback;
 
 
 public class StaffChatCommands implements CommandClass {
@@ -22,11 +34,48 @@ public class StaffChatCommands implements CommandClass {
     @Inject
     private UserHandler settingsHandler;
 
-    @Command(names = {"staffchat", "sc"}, usage = "Usage: /<command>", max = 0, onlyPlayer = true, permission = "base.command.staffchat")
-    public boolean staffChatCommand(Player sender, CommandContext context) {
+    private Channel<StaffChatMessage> messageChannel;
+
+    @Inject
+    StaffChatCommands(Messenger messenger) {
+        messageChannel = messenger.getChannel("staffChat", StaffChatMessage.class);
+    }
+
+    @Command(names = {"staffchat", "sc"}, usage = "Usage: /<command> [text]", permission = "base.command.staffchat")
+    public boolean staffChatCommand(CommandSender commandSender, CommandContext context) {
+        if (context.getArguments().size() >= 1) {
+            String stringMessage = context.getJoinedArgs(0);
+
+            if (commandSender.hasPermission("base.staffchat.color")) {
+                stringMessage = ChatColor.translateAlternateColorCodes('&', stringMessage);
+            }
+
+            String staffFormattedMessage = MessageFormat.format(i18n.translate("chat.staff.format"), commandSender.getName(), stringMessage);
+
+            Bukkit.getConsoleSender().sendMessage(staffFormattedMessage);
+
+            Set<String> userIds = Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission("base.command.staffchat.see")).map(Player::getUniqueId).map(UUID::toString).collect(Collectors.toSet());
+
+            addCallback(settingsHandler.find(userIds, userIds.size()), userSettingsSet -> {
+                userSettingsSet.stream().filter(Objects::nonNull).filter(User.ChatSettings::isStaffChatVisible).map(playerSettings -> Bukkit.getPlayer(playerSettings.getUUID())).forEach(player -> player.sendMessage(staffFormattedMessage));
+            });
+
+            StaffChatMessage message = new StaffChatMessage(commandSender.getName(), stringMessage);
+
+            messageChannel.sendMessage(message);
+
+            return true;
+        }
+
+        if (!(commandSender instanceof Player)) {
+            return false;
+        }
+
+        Player sender = (Player) commandSender;
+
         Optional<User.Complete> optionalSettings = Optional.ofNullable(settingsHandler.findOneSync(sender.getUniqueId().toString()));
 
-        if(!optionalSettings.isPresent()){
+        if (!optionalSettings.isPresent()) {
             return true;
         }
 
@@ -46,7 +95,7 @@ public class StaffChatCommands implements CommandClass {
     public boolean toggleStaffChat(Player sender, CommandContext context) {
         Optional<User.Complete> optionalSettings = Optional.ofNullable(settingsHandler.findOneSync(sender.getUniqueId().toString()));
 
-        if(!optionalSettings.isPresent()){
+        if (!optionalSettings.isPresent()) {
             return true;
         }
 
