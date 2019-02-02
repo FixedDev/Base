069994305076e06f8961af48c8230beec4f44276
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-02-01 21:54:08 -0600

    Added old serialized user deserialization check

--- src/test/java/us/sparknetwork/test/base/user/BaseUserSerializationTest.java
+++ src/test/java/us/sparknetwork/test/base/user/BaseUserSerializationTest.java
@@ -3,8 +3,8 @@ package us.sparknetwork.test.base.user;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
-import us.sparknetwork.base.handlers.user.BaseUser;
-import us.sparknetwork.base.handlers.user.User;
+import us.sparknetwork.base.user.BaseUser;
+import us.sparknetwork.base.user.User;
 
 import java.util.UUID;
 
@@ -12,10 +12,11 @@ public class BaseUserSerializationTest {
 
     private static UUID userId = UUID.fromString("6add6e1d-1766-4370-b25d-9ee8d8fb45b9");
 
-    private static String serializedUser = "{\"state\":{\"godModeEnabled\":false,\"freezed\":false,\"vanished\":false},\"nameHistory\":[],\"chatData\":{\"lastSpeakTime\":0},\"chatSettings\":{\"staffChatVisible\":false,\"globalChatVisible\":true,\"inStaffChat\":false},\"whisperData\":{\"lastPrivateMessageReplier\":null},\"nick\":null,\"whisperSettings\":{\"socialSpyVisible\":false,\"ignoredPlayers\":[]},\"connectionData\":{\"online\":false,\"lastJoin\":0,\"lastServerId\":null},\"addressHistoryData\":{\"addressHistory\":[]},\"_id\":\"6add6e1d-1766-4370-b25d-9ee8d8fb45b9\"}";
+    private String oldSerializedUser = "{\"nick\":null,\"nameHistory\":[],\"lastJoin\":0,\"lastServerId\":null,\"online\":false,\"addressHistory\":[],\"lastSpeakTime\":0,\"globalChatVisible\":true,\"staffChatVisible\":false,\"inStaffChat\":false,\"lastPrivateMessageReplier\":null,\"socialSpyVisible\":false,\"ignoredPlayers\":[],\"privateMessagesVisible\":true,\"godModeEnabled\":false,\"vanished\":false,\"freezed\":false,\"friendsLimit\" : 0, \"friends\": [], \"_id\":\"6add6e1d-1766-4370-b25d-9ee8d8fb45b9\"}";
+    private String serializedUser = "{\"nick\":null,\"nameHistory\":[],\"lastJoin\":0,\"lastServerId\":null,\"online\":false,\"addressHistory\":[],\"lastSpeakTime\":0,\"globalChatVisible\":true,\"staffChatVisible\":false,\"inStaffChat\":false,\"lastPrivateMessageReplier\":null,\"socialSpyVisible\":false,\"ignoredPlayers\":[],\"privateMessagesVisibility\": \"ALL\",\"godModeEnabled\":false,\"vanished\":false,\"freezed\":false,\"friendsLimit\" : 0, \"friends\": [], \"_id\":\"6add6e1d-1766-4370-b25d-9ee8d8fb45b9\"}";
 
     @Test
-    public void serializeBaseUser() throws Exception{
+    public void serializeBaseUser() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
 
         mapper.setVisibility(mapper.getSerializationConfig()
@@ -28,7 +29,22 @@ public class BaseUserSerializationTest {
 
         User user = new BaseUser(userId);
 
-        mapper.writeValue(System.out, user);
+        mapper.writerWithDefaultPrettyPrinter().writeValue(System.out, user);
+    }
+
+    @Test
+    public void deserializeOldVersionBaseUser() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.setVisibility(mapper.getSerializationConfig()
+                .getDefaultVisibilityChecker()
+                .withFieldVisibility(JsonAutoDetect.Visibility.NONE)
+                .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
+                .withIsGetterVisibility(JsonAutoDetect.Visibility.ANY)
+                .withSetterVisibility(JsonAutoDetect.Visibility.ANY)
+                .withCreatorVisibility(JsonAutoDetect.Visibility.PROTECTED_AND_PUBLIC));
+
+        User user = mapper.readValue(oldSerializedUser, User.class);
     }
 
     @Test
