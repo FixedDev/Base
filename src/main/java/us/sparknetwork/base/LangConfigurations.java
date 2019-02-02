8b03c6cc4f9e6d6a94de500971409a25a1e28acb
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-02-01 19:51:15 -0600

    Added parseWhisperVisibility method

--- src/main/java/us/sparknetwork/base/LangConfigurations.java
+++ src/main/java/us/sparknetwork/base/LangConfigurations.java
@@ -1,19 +1,34 @@
 package us.sparknetwork.base;
 
+import us.sparknetwork.base.user.User;
+
 public class LangConfigurations {
 
-    public static String tl(I18n i18n, String path){
-        if(i18n == null){
+    public static String tl(I18n i18n, String path) {
+        if (i18n == null) {
             throw new IllegalStateException("I18n instance is null!");
         }
         return i18n.translate(path);
     }
 
-    public static String parseVisibility(I18n i18n, boolean bool){
-        return bool ? tl(i18n,"visible") : tl(i18n,"invisible");
+    public static String parseWhisperVisibility(I18n i18n, User.WhisperVisibility visibility) {
+        switch (visibility) {
+            case ALL:
+                return tl(i18n, "whisper.visibility.all");
+            case FRIENDS:
+                return tl(i18n, "whisper.visibility.friends");
+            case NONE:
+                return tl(i18n, "whisper.visibility.none");
+            default:
+                return "";
+        }
     }
 
-    public static String convertBoolean(I18n i18n, boolean bool){
-        return bool ? tl(i18n,"true") : tl(i18n,"false");
+    public static String parseVisibility(I18n i18n, boolean bool) {
+        return bool ? tl(i18n, "visible") : tl(i18n, "invisible");
+    }
+
+    public static String convertBoolean(I18n i18n, boolean bool) {
+        return bool ? tl(i18n, "true") : tl(i18n, "false");
     }
 }
