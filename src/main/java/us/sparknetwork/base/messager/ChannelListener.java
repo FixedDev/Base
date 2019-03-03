9e93ee8a9189c27b10c6be40e8f97e52fc59e943
Gilberto Garcia(Ggamer55) <gilbertodamian14@gmail.com>
2019-03-03 12:30:09 -0600

    Remove an unused import

--- src/main/java/us/sparknetwork/base/messager/ChannelListener.java
+++ src/main/java/us/sparknetwork/base/messager/ChannelListener.java
@@ -1,7 +1,5 @@
 package us.sparknetwork.base.messager;
 
-import java.util.UUID;
-
 public interface ChannelListener<T> {
     void onMessageReceived(String channel, String serverSenderId, T data);
 }
