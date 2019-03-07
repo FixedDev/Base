ad4ea564b246d494797c4dbcd13b0e45b50a6203
Gilberto Garcia(Ggamer55) <gilbertodamian14@gmail.com>
2019-03-06 23:58:38 -0600

    Miss to register the IdGenerator whoops

--- src/main/java/us/sparknetwork/base/inject/HandlersModule.java
+++ src/main/java/us/sparknetwork/base/inject/HandlersModule.java
@@ -1,6 +1,7 @@
 package us.sparknetwork.base.inject;
 
 import com.google.inject.AbstractModule;
+import us.sparknetwork.base.id.IdGeneratorModule;
 import us.sparknetwork.base.punishment.PunishmentManagerModule;
 import us.sparknetwork.base.restart.RestartModule;
 import us.sparknetwork.base.server.MongoServerManager;
@@ -29,6 +30,7 @@ public class HandlersModule extends AbstractModule {
         bind(UserHandler.class).to(BaseUserHandler.class);
         bind(FriendRequestHandler.class).to(BaseFriendRequestHandler.class);
         bind(WhisperManager.class).to(WhisperManagerImpl.class);
+        install(new IdGeneratorModule());
         install(new RestartModule());
         install(new PunishmentManagerModule());
     }
