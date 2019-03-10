92cc34e54b17d586abd9b34f69d5456ca4a25046
Gilberto Garcia(Ggamer55) <gilbertodamian14@gmail.com>
2019-03-09 18:36:56 -0600

    Add more freaking commands

--- src/main/java/us/sparknetwork/base/command/essentials/PunishmentCommands.java
+++ src/main/java/us/sparknetwork/base/command/essentials/PunishmentCommands.java
@@ -8,8 +8,8 @@ import me.ggamer55.bcm.parametric.annotation.Parameter;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import us.sparknetwork.base.I18n;
+import us.sparknetwork.base.StaffPriority;
 import us.sparknetwork.base.punishment.PunishmentManager;
 import us.sparknetwork.base.punishment.PunishmentType;
 import us.sparknetwork.base.user.UserHandler;
@@ -31,16 +31,24 @@ public class PunishmentCommands implements CommandClass {
 
     private static final String NO_PERMISSION_MESSAGE = ChatColor.RED + "No permission.";
 
+
+
     @Command(names = {"ban", "tempban"}, permission = "base.command.ban", min = 1, usage = "/<command> <target> [duration] [reason] [-s]")
     public boolean banPlayer(CommandSender sender, CommandContext context, OfflinePlayer target, @Parameter(value = "s", isFlag = true) boolean silent) {
-
         if (!target.isOnline() && !sender.hasPermission("base.command.ban.offline")) {
             sender.sendMessage(NO_PERMISSION_MESSAGE);
 
             return true;
         }
 
-        if (target.isOnline() && target.getPlayer().hasPermission("base.command.ban.bypass") && sender instanceof Player) {
+        StaffPriority senderPriority = StaffPriority.getByCommandSender(sender);
+        StaffPriority targetPriority = StaffPriority.NONE;
+
+        if(target.isOnline()){
+            targetPriority = StaffPriority.getByCommandSender(target.getPlayer());
+        }
+
+        if(targetPriority.isMoreThan(senderPriority)){
             sender.sendMessage(NO_PERMISSION_MESSAGE);
 
             return true;
@@ -132,6 +140,230 @@ public class PunishmentCommands implements CommandClass {
 
         ListenableFutureUtils.addCallback(userHandler.findOne(target.getUniqueId().toString()), data ->
                 punishmentManager.createPunishment(PunishmentType.BAN, sender, data, reason, null, false, silent)
+        );
+
+        return true;
+    }
+
+    @Command(names = {"ipban", "tempipban"}, permission = "base.command.ban", min = 1, usage = "/<command> <target> [duration] [reason] [-s]")
+    public boolean ipbanPlayer(CommandSender sender, CommandContext context, OfflinePlayer target, @Parameter(value = "s", isFlag = true) boolean silent) {
+        if (!target.isOnline() && !sender.hasPermission("base.command.ipban.offline")) {
+            sender.sendMessage(NO_PERMISSION_MESSAGE);
+
+            return true;
+        }
+
+        StaffPriority senderPriority = StaffPriority.getByCommandSender(sender);
+        StaffPriority targetPriority = StaffPriority.NONE;
+
+        if(target.isOnline()){
+            targetPriority = StaffPriority.getByCommandSender(target.getPlayer());
+        }
+
+        if(targetPriority.isMoreThan(senderPriority)){
+            sender.sendMessage(NO_PERMISSION_MESSAGE);
+
+            return true;
+        }
+
+        ListenableFutureUtils.addCallback(punishmentManager.getLastPunishment(PunishmentType.BAN, target.getUniqueId(), null), oldPunishment -> {
+            if (oldPunishment != null) {
+                long secondsLeft = 0;
+
+                if (!oldPunishment.isPermanent() && oldPunishment.getEndDate() != null) {
+                    secondsLeft = ZonedDateTime.now().until(oldPunishment.getEndDate(), ChronoUnit.SECONDS);
+                }
+
+                if ((oldPunishment.isPermanent() || secondsLeft >= 0) && !sender.hasPermission("base.command.ipban.override")) {
+                    sender.sendMessage(NO_PERMISSION_MESSAGE);
+
+                    return;
+                }
+
+                oldPunishment.setActive(false);
+                punishmentManager.savePunishment(oldPunishment);
+            }
+        });
+
+        if (context.getArgumentsLength() == 1) {
+            // Only the target is provided, create a permanent ipban if the sender has permission
+            if (!sender.hasPermission("base.command.ipban.permanent")) {
+                sender.sendMessage(NO_PERMISSION_MESSAGE);
+                return true;
+            }
+
+            ListenableFutureUtils.addCallback(userHandler.findOne(target.getUniqueId().toString()), data ->
+                    punishmentManager.createPunishment(PunishmentType.BAN, sender, data, i18n.translate("punishment.default.reason"), null, true, silent)
+            );
+
+            return true;
+        }
+
+        // We have more than 1 args, so, the first argument provided should be the target, the second the duration, and the extras should be the reason
+        String stringDuration = context.getArgument(1);
+        long durationInMillis;
+
+        // Checking if the duration is valid
+        try {
+            // Well is valid
+            durationInMillis = DateUtil.parseStringDuration(stringDuration);
+        } catch (NumberFormatException ex) {
+            // Is invalid, also we should use a different number than 0
+            durationInMillis = 0L;
+        }
+
+        // Checking if the duration is more than 0 millis
+        if (durationInMillis > 0) {
+            // It is, so execute all the code for temporal ipbans
+            if (!sender.hasPermission("base.command.ipban.temporal")) {
+                sender.sendMessage(NO_PERMISSION_MESSAGE);
+
+                return true;
+            }
+
+            String reason = i18n.translate("punishment.default.reason");
+
+            if (context.getArgumentsLength() > 2) {
+                reason = context.getJoinedArgs(2);
+            }
+
+            final String ipbanReason = reason;
+
+            Duration ipbanDuration = Duration.ofMillis(durationInMillis);
+
+            final ZonedDateTime ipbanEndDate = ZonedDateTime.now().plus(ipbanDuration);
+
+            ListenableFutureUtils.addCallback(userHandler.findOne(target.getUniqueId().toString()), data ->
+                    punishmentManager.createPunishment(PunishmentType.BAN, sender, data, ipbanReason, ipbanEndDate, true, silent)
+            );
+
+            return true;
+        }
+
+        // The target is provided, a duration is also provided, but is invalid, it will be included as part of the reason
+
+        // This is again checking if the sender has permission for permanent ipbans
+        if (!sender.hasPermission("base.command.ipban.permanent")) {
+            sender.sendMessage(NO_PERMISSION_MESSAGE);
+            return true;
+        }
+
+        String reason = context.getJoinedArgs(1);
+
+        ListenableFutureUtils.addCallback(userHandler.findOne(target.getUniqueId().toString()), data ->
+                punishmentManager.createPunishment(PunishmentType.BAN, sender, data, reason, null, true, silent)
+        );
+
+        return true;
+    }
+
+    @Command(names = {"mute", "tempmute"}, permission = "base.command.mute", min = 1, usage = "/<command> <target> [duration] [reason] [-s]")
+    public boolean mutePlayer(CommandSender sender, CommandContext context, OfflinePlayer target, @Parameter(value = "s", isFlag = true) boolean silent) {
+        if (!target.isOnline() && !sender.hasPermission("base.command.mute.offline")) {
+            sender.sendMessage(NO_PERMISSION_MESSAGE);
+
+            return true;
+        }
+
+        StaffPriority senderPriority = StaffPriority.getByCommandSender(sender);
+        StaffPriority targetPriority = StaffPriority.NONE;
+
+        if(target.isOnline()){
+            targetPriority = StaffPriority.getByCommandSender(target.getPlayer());
+        }
+
+        if(targetPriority.isMoreThan(senderPriority)){
+            sender.sendMessage(NO_PERMISSION_MESSAGE);
+
+            return true;
+        }
+
+        ListenableFutureUtils.addCallback(punishmentManager.getLastPunishment(PunishmentType.MUTE, target.getUniqueId(), null), oldPunishment -> {
+            if (oldPunishment != null) {
+                long secondsLeft = 0;
+
+                if (!oldPunishment.isPermanent() && oldPunishment.getEndDate() != null) {
+                    secondsLeft = ZonedDateTime.now().until(oldPunishment.getEndDate(), ChronoUnit.SECONDS);
+                }
+
+                if ((oldPunishment.isPermanent() || secondsLeft >= 0) && !sender.hasPermission("base.command.mute.override")) {
+                    sender.sendMessage(NO_PERMISSION_MESSAGE);
+
+                    return;
+                }
+
+                oldPunishment.setActive(false);
+                punishmentManager.savePunishment(oldPunishment);
+            }
+        });
+
+        if (context.getArgumentsLength() == 1) {
+            // Only the target is provided, create a permanent mute if the sender has permission
+            if (!sender.hasPermission("base.command.mute.permanent")) {
+                sender.sendMessage(NO_PERMISSION_MESSAGE);
+                return true;
+            }
+
+            ListenableFutureUtils.addCallback(userHandler.findOne(target.getUniqueId().toString()), data ->
+                    punishmentManager.createPunishment(PunishmentType.MUTE, sender, data, i18n.translate("punishment.default.reason"), null, false, silent)
+            );
+
+            return true;
+        }
+
+        // We have more than 1 args, so, the first argument provided should be the target, the second the duration, and the extras should be the reason
+        String stringDuration = context.getArgument(1);
+        long durationInMillis;
+
+        // Checking if the duration is valid
+        try {
+            // Well is valid
+            durationInMillis = DateUtil.parseStringDuration(stringDuration);
+        } catch (NumberFormatException ex) {
+            // Is invalid, also we should use a different number than 0
+            durationInMillis = 0L;
+        }
+
+        // Checking if the duration is more than 0 millis
+        if (durationInMillis > 0) {
+            // It is, so execute all the code for temporal mutes
+            if (!sender.hasPermission("base.command.mute.temporal")) {
+                sender.sendMessage(NO_PERMISSION_MESSAGE);
+
+                return true;
+            }
+
+            String reason = i18n.translate("punishment.default.reason");
+
+            if (context.getArgumentsLength() > 2) {
+                reason = context.getJoinedArgs(2);
+            }
+
+            final String muteReason = reason;
+
+            Duration muteDuration = Duration.ofMillis(durationInMillis);
+
+            final ZonedDateTime muteEndDate = ZonedDateTime.now().plus(muteDuration);
+
+            ListenableFutureUtils.addCallback(userHandler.findOne(target.getUniqueId().toString()), data ->
+                    punishmentManager.createPunishment(PunishmentType.MUTE, sender, data, muteReason, muteEndDate, false, silent)
+            );
+
+            return true;
+        }
+
+        // The target is provided, a duration is also provided, but is invalid, it will be included as part of the reason
+
+        // This is again checking if the sender has permission for permanent mutes
+        if (!sender.hasPermission("base.command.mute.permanent")) {
+            sender.sendMessage(NO_PERMISSION_MESSAGE);
+            return true;
+        }
+
+        String reason = context.getJoinedArgs(1);
+
+        ListenableFutureUtils.addCallback(userHandler.findOne(target.getUniqueId().toString()), data ->
+                punishmentManager.createPunishment(PunishmentType.MUTE, sender, data, reason, null, false, silent)
         );
 
         return true;
