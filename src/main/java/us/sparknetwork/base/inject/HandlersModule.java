ab2111d8e529e027eae6bd686658b0d86468a5c8
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-04-30 22:38:48 -0500

    Delete the install of the RestartModule and delete in favour of RestartManagerModule
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- src/main/java/us/sparknetwork/base/inject/HandlersModule.java
+++ src/main/java/us/sparknetwork/base/inject/HandlersModule.java
@@ -3,9 +3,6 @@ package us.sparknetwork.base.inject;
 import com.google.inject.AbstractModule;
 import us.sparknetwork.base.id.IdGeneratorModule;
 import us.sparknetwork.base.punishment.PunishmentManagerModule;
-import us.sparknetwork.base.restart.RestartModule;
-import us.sparknetwork.base.server.MongoServerManager;
-import us.sparknetwork.base.server.ServerManager;
 import us.sparknetwork.base.user.BaseUserHandler;
 import us.sparknetwork.base.user.UserHandler;
 import us.sparknetwork.base.user.finder.UserFinder;
@@ -24,14 +21,12 @@ public class HandlersModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Messenger.class).to(RedisMessenger.class);
-        bind(ServerManager.class).to(MongoServerManager.class);
         bind(UserFinder.class).to(UserFinderImpl.class);
         bind(ItemDb.class).to(SimpleItemDb.class);
         bind(UserHandler.class).to(BaseUserHandler.class);
         bind(FriendRequestHandler.class).to(BaseFriendRequestHandler.class);
         bind(WhisperManager.class).to(WhisperManagerImpl.class);
         install(new IdGeneratorModule());
-        install(new RestartModule());
         install(new PunishmentManagerModule());
     }
 }
