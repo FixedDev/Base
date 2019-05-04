686066d869d703313a08dd786acae761f68ca2a0
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-05-03 21:30:21 -0500

    As UserFinderImpl was moved from user.finder.impl to user.finder the import changed
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- src/main/java/us/sparknetwork/base/inject/HandlersModule.java
+++ src/main/java/us/sparknetwork/base/inject/HandlersModule.java
@@ -6,7 +6,7 @@ import us.sparknetwork.base.punishment.PunishmentManagerModule;
 import us.sparknetwork.base.user.BaseUserHandler;
 import us.sparknetwork.base.user.UserHandler;
 import us.sparknetwork.base.user.finder.UserFinder;
-import us.sparknetwork.base.user.finder.impl.UserFinderImpl;
+import us.sparknetwork.base.user.finder.UserFinderImpl;
 import us.sparknetwork.base.user.friends.BaseFriendRequestHandler;
 import us.sparknetwork.base.user.friends.FriendRequestHandler;
 import us.sparknetwork.base.whisper.WhisperManager;
