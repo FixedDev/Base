bb5a61fe0ca79e5243e15ab5c39ad46df2d309f2
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-06-20 14:31:25 -0500

    Added getLocalServer method
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- src/main/java/us/sparknetwork/base/server/ServerManager.java
+++ src/main/java/us/sparknetwork/base/server/ServerManager.java
@@ -1,9 +1,9 @@
 package us.sparknetwork.base.server;
 
 import me.fixeddev.service.PluginService;
-import me.fixeddev.service.Service;
 import us.sparknetwork.base.datamanager.StorageProvider;
 import us.sparknetwork.base.server.type.Server;
 
 public interface ServerManager extends StorageProvider<Server, Server>, PluginService {
+    LocalServerData getLocalServer();
 }
