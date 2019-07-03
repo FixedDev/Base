39319a5c6cafa1ed8368ffc0f9e1474a825a8fc5
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-07-03 18:06:14 -0500

    Add a joinTime and make everything except the serverId immutable
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- src/main/java/us/sparknetwork/base/user/session/Session.java
+++ src/main/java/us/sparknetwork/base/user/session/Session.java
@@ -3,11 +3,14 @@ package us.sparknetwork.base.user.session;
 import org.jetbrains.annotations.NotNull;
 import us.sparknetwork.base.datamanager.Model;
 
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 public interface Session extends Model {
     @NotNull
     UUID playerId();
+
+    ZonedDateTime joinTime();
 
     @NotNull
     String serverId();
--- src/main/java/us/sparknetwork/base/user/session/SessionImpl.java
+++ src/main/java/us/sparknetwork/base/user/session/SessionImpl.java
@@ -3,17 +3,25 @@ package us.sparknetwork.base.user.session;
 import lombok.AllArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @AllArgsConstructor
 public class SessionImpl implements Session {
-    private UUID playerId;
+    private final UUID playerId;
+    private final ZonedDateTime joinTime;
     private String serverId;
 
     @NotNull
     @Override
     public UUID playerId() {
         return playerId;
+    }
+
+    @Override
+    @NotNull
+    public ZonedDateTime joinTime() {
+        return joinTime;
     }
 
     @NotNull
